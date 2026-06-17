9f1b9f03a5c0470ffee7da278c52c22048d1e0b4
Abdur Rehman Khan <abdurrehmankhan0909@gmail.com>
2026-06-18 01:10:24 +0500

    feat: add JS basics/variables

---
+++ 01_basics/variables.js
@@ -0,0 +1,7 @@
+// Variables
+var oldWay = "avoid this"; // function scoped, avoid
+let name = "Abdur Rehman"; // block scoped, reassignable
+const university = "FAST-NUCES"; // block scoped, constant
+
+console.log("Name:", name);
+console.log("University:", university);
