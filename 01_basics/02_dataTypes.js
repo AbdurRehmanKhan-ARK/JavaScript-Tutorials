b4e56cd9095d1a6af7e0a729cd92a922fb6d41e2
Abdur Rehman Khan <abdurrehmankhan0909@gmail.com>
2026-06-18 23:06:14 +0500

    feat: 02_dataTypes - basic data types covered

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version
+/*
+alert("Hello"); // a pop up window with message
+we are using nodeJS not browser so alert will not work here
+plus semicolons aren't that much important as readability is disturbed but it's a good practice to use them
+*/
+console.log("Hello"); console.log(4-2)
+// follow ECMAscript to follow best practices and avoid errors in javascript
+
+// let's come to data types now
+
+
+/*
+PREMITIVE DATA TYPES
+number => 2 to power 53
+bigint
+string => "Abdur Rehman"
+boolean => true or false
+null => empty value / standalone value
+undefined => value ain't assigned yet
+symbol => unique value (mostly used in react)
+
+OBJECTS
+*/
+let age = 20
+console.log(typeof age) // typeof operator => check the data type of a variable
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
